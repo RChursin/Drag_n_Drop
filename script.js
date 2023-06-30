cab28a676c10bc9ca453124df4195ae4c87339ef
ganstacoder <ganstacoder@gmail.com>
2023-06-30 16:43:08 -0700

    bug fix

--- script.js
+++ script.js
@@ -87,18 +87,18 @@ function updateDOM() {
         createItemEl(progressListEl, 1, progressItem, index);
     });
     progressListArray = filterArray(progressListArray);
-    // On Hold Column
-    onHoldListEl.textContent = '';
-    onHoldListArray.forEach((onHoldItem, index) => {
-        createItemEl(onHoldListEl, 2, onHoldItem, index);
-    });
-    onHoldListArray = filterArray(onHoldListArray);
     // Complete Column
     completeListEl.textContent = '';
     completeListArray.forEach((completeItem, index) => {
-        createItemEl(completeListEl, 3, completeItem, index);
+        createItemEl(completeListEl, 2, completeItem, index);
     });
     completeListArray = filterArray(completeListArray);
+    // On Hold Column
+    onHoldListEl.textContent = '';
+    onHoldListArray.forEach((onHoldItem, index) => {
+        createItemEl(onHoldListEl, 3, onHoldItem, index);
+    });
+    onHoldListArray = filterArray(onHoldListArray);
     // Don't run more than once, Update Local Storage
     updatedOnLoad = true;
     updateSavedColumns();
@@ -152,13 +152,13 @@ function rebuildArrays() {
     for (let i = 0; i < progressListEl.children.length; i++) {
         progressListArray.push(progressListEl.children[i].textContent);
     }
-    onHoldListArray = [];
-    for (let i = 0; i < onHoldListEl.children.length; i++) {
-        onHoldListArray.push(onHoldListEl.children[i].textContent);
-    }
     completeListArray = [];
     for (let i = 0; i < completeListEl.children.length; i++) {
         completeListArray.push(completeListEl.children[i].textContent);
+    }
+    onHoldListArray = [];
+    for (let i = 0; i < onHoldListEl.children.length; i++) {
+        onHoldListArray.push(onHoldListEl.children[i].textContent);
     }
     updateDOM();
 }
